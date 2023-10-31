4f673d6fa255d48f119bb2b6202e563fc7b175f2
MarioWindsor <mario.windsor@gmail.com>
2023-10-31 19:57:14 +0530

    init admin console files

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
+const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("css"); // Copy `css/` to `_site/css`
 	eleventyConfig.addPassthroughCopy("media"); // Copy `media/` to `_site/media`
+	eleventyConfig.addPassthroughCopy("admin"); // Copy `admin/` to `_site/admin`
+
+	eleventyConfig.addFilter("postDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+	});
+
 };
