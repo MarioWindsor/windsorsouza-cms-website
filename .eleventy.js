99fb44b957cccdeec6ba1ac717cfa71b22def2cc
MarioWindsor <mario.windsor@gmail.com>
2023-11-08 18:56:44 +0530

    BugFix: CMS Media Routing

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("css"); // Copy `css/` to `_site/css`
 	eleventyConfig.addPassthroughCopy("media"); // Copy `media/` to `_site/media`
 	eleventyConfig.addPassthroughCopy("admin"); // Copy `admin/` to `_site/admin`
+	eleventyConfig.addPassthroughCopy("media/uploads"); // Copy `admin/` to `_site/admin`
 
 	eleventyConfig.addFilter("postDate", (dateObj) => {
 		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
