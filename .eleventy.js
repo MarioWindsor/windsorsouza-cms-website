6f89d9ac2fc9826c673753e98e2368c790a0148c
MarioWindsor <mario.windsor@gmail.com>
2023-11-08 18:46:44 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const { DateTime } = require("luxon");
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("css"); // Copy `css/` to `_site/css`
 	eleventyConfig.addPassthroughCopy("media"); // Copy `media/` to `_site/media`
+	eleventyConfig.addPassthroughCopy("media/uploads"); // Copy `media/` to `_site/media`
 	eleventyConfig.addPassthroughCopy("admin"); // Copy `admin/` to `_site/admin`
 
 	eleventyConfig.addFilter("postDate", (dateObj) => {
