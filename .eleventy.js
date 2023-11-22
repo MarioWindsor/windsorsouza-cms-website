42229b513e8e63383a0bd2b17a38bcd8e1b73444
MarioWindsor <mario.windsor@gmail.com>
2023-11-22 18:45:02 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("media"); // Copy `media/` to `_site/media`
 	eleventyConfig.addPassthroughCopy("admin"); // Copy `admin/` to `_site/admin`
 	eleventyConfig.addPassthroughCopy("js"); // Copy `admin/` to `_site/admin`
+	eleventyConfig.addPassthroughCopy("demo"); // Copy `admin/` to `_site/admin`
 
 	eleventyConfig.addFilter("postDate", (dateObj) => {
 		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
