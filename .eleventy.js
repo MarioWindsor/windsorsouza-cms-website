2384637aad0e6b76b396a1503406d67b0a19475b
MarioWindsor <mario.windsor@gmail.com>
2024-03-12 20:09:49 +0530

    Downloads: Company Profile

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("media"); // Copy `media/` to `_site/media`
 	eleventyConfig.addPassthroughCopy("admin"); // Copy `admin/` to `_site/admin`
 	eleventyConfig.addPassthroughCopy("js"); // Copy `admin/` to `_site/admin`
-	eleventyConfig.addPassthroughCopy("demo"); // Copy `admin/` to `_site/admin`
+	eleventyConfig.addPassthroughCopy("download"); // Copy `download/` to `_site/download`
 
 	eleventyConfig.addFilter("postDate", (dateObj) => {
 		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
